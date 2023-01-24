6f6e396393c000066440562d1c88dbd5135ca2ed
Matteriel-Git <98388545+Matteriel-Git@users.noreply.github.com>
2023-01-24 12:25:12 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,93 @@
-document.querySelector('#push').onclick = function() {
-    if(document.querySelector('#newtask input').value.length == 0) {
-        alert("Please Enter a Task")
-    }
-    else {
-        document.querySelector('#tasks').innerHTML +=
-        <div class="task">
-        <span id="taskname">
-        ${document.querySelector('#newtask input').value}
-        </span>
-        <button class="delete">
-        <i class="far fa-trash-alt"></i>
-        </button>
-        </div>
-        ;
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i = 0; i < current_tasks.length; i++) {
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
+const todoForm = document.querySelector('.todo-form');
+const todoInput = document.querySelector('.todo-input');
+const todoItemsList = document.querySelector('.todo-items');
+
+let todos = [];
+
+todoForm.addEventListener('submit',
+function(event) {
+    event.preventDefault();
+    addTodo(todoInput.value);
+});
+
+function addTodo(item) {
+    if (item !== '') {
+
+        const todo = {
+            id: Date.now(),
+            name: item,
+            completed: false,
+        };
+
+        todos.push(todo);
+        addToLocalStorage(todos);
+
+        todoInput.value = '';
     }
 }
+
+function renderTodos(todos) {
+    todoItemsList.innerHTML = '';
+
+    todos.forEach(function(item) {
+        const checked = item.completed ? 'checked': null;
+
+        const li = document.createElement('li');
+
+        li.setAttribute('class', 'item');
+        li.setAttribute('data-key', item.id);
+
+        if(item.completed == true) {
+            li.classList.add('checked');
+        }
+        
+        li.innerHTML = `
+        <input type="checkbox" class="checkbox" ${checked}>
+        ${item.name}
+        <button class="delete-button">X</button>
+        `;
+
+        todoItemsList.append(li);
+    });
+}
+
+function addToLocalStorage(todos) {
+    localStorage.setItem('todos', JSON.stringify(todos));
+
+    renderTodos(todos);
+}
+
+function getFormLocalStorage() {
+    const reference = localStorage.getItem('todos');
+
+    if(reference) {
+        todos = JSON.parse(reference);
+        renderTodos(todos);
+    }
+}
+
+function toggle(id) {
+    todos.forEach(function(item) {
+        if(item.id == id) {
+            item.completed = !item.completed;
+        }
+    });
+
+    addToLocalStorage(todos);
+}
+
+function deleteTodo(id) {
+    todos = todos.filter(function(item) {
+        return item.id != id;
+    });
+
+    addToLocalStorage();
+}
+
+getFormLocalStorage();
+
+todoItemsList.addEventListener('click', function(event) {
+    if(event.target.type === '(checkbox') {
+        toggle(event.target.parentElement.getAttribute('data-key'));
+    }
+});
